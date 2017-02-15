badb4cde883ee5dafe6f17c17c7f201bb6d7df9a
iceroller <iceman_R@list.ru>
2017-02-15 17:05:37 +0200

    new files

--- partials/mid.php
+++ partials/mid.php
@@ -1,11 +1,11 @@
 <!-- mid start -->
 <div class="mid">
     <div class="title"><h5>Office hours:</h5></div>
-    <dl>
-        <dt>Mon-Fre:</dt><dd>7:00 am - 7:00 pm</dd><br>
-        <dt>Saturday:</dt><dd>7:00 am - 5:00 pm</dd><br>
+    <div class="dlWrap"><dl>
+        <dt>Mon-Fre:</dt><dd>7:00 am - 7:00 pm <br /></dd>
+        <dt>Saturday:</dt><dd>7:00 am - 5:00 pm <br /></dd>
         <dt>Sunday:</dt><dd>7:00 am - 3:00 pm</dd>
-        <span><a href="#">Online orders accepted 24/7</a></span>
     </dl>
+    <span><a href="#">Online orders accepted 24/7</a></span></div>
 </div>
 <!-- mid end -->
